c919c0a577a7fccde8629e1dae582fe20acfab1b
IT24102876 <IT24102876@my.sliit.lk>
2025-10-01 09:26:54 +0530

    counselor codes

--- src/main/java/org/example/unihelpdesk/controller/CounselorController.java
+++ src/main/java/org/example/unihelpdesk/controller/CounselorController.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.example.unihelpdesk.model.User;
 import org.example.unihelpdesk.service.TicketService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model  ;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
